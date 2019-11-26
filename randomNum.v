0b9df4a99407c1c6e54e57ca967a6d03e3ce54f3
kevry <45439265+kevry@users.noreply.github.com>
2019-11-26 14:22:33 -0500

    Create randomNum.v

---
+++ randomNum.v
@@ -0,0 +1,18 @@
+module randomNum(clk, reset, number);
+input clk, reset;
+output reg [2:0]number;
+
+reg [2:0]next;
+wire feedback = number[2]^number[1];
+
+always@(posedge clk or posedge reset) begin
+if (reset)
+number <= 3'b001;
+else
+number <= next;
+end
+
+always@(*) begin
+next <= {number[1:0], feedback}; 
+end
+endmodule
