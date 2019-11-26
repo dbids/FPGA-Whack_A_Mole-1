137a51de96e86d2879d97b57d20afd55f1cc4ed8
atlascu <54875107+atlascu@users.noreply.github.com>
2019-11-26 16:57:40 -0500

    Update randomNum.v

--- randomNum.v
+++ randomNum.v
@@ -5,14 +5,15 @@ output reg [2:0]number;
 reg [2:0]next;
 wire feedback = number[2]^number[1];
 
-always@(posedge clk or posedge reset) begin
-if (reset)
-number <= 3'b001;
-else
-number <= next;
-end
+  always@(posedge clk or posedge reset) begin
+   if (reset)
+      number <= 3'b001;
+   else
+      number <= next;
+   end //always statement
 
-always@(*) begin
-next <= {number[1:0], feedback}; 
-end
+  always@(*) begin
+    next <= {number[1:0], feedback}; 
+  end //always statement
+  
 endmodule
