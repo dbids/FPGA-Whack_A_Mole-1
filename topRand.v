29985619577ee72fad5a8ffe52f921d3077a0401
dbids <dbids@bu.edu>
2019-11-26 19:00:56 -0500

    Updated clock module name for use in other top module

--- topRand.v
+++ topRand.v
@@ -5,7 +5,7 @@ output [4:0]displayL;
 wire clk_out;
 wire [2:0]number;
 
-  clock_divider c1(.clk(clk), .reset(reset), .clk_out(clk_out));
+  clock_divider_1Hz c1(.clk(clk), .reset(reset), .clk_out(clk_out));
   randomNum rn(.clk_out(clk_out), .reset(reset), .number(number));
   LEDdisplay ld(.number(number), .displayL(displayL));
 
