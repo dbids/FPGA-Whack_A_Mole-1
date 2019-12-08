fdeccf33106a82af078503c28f455f96f12668ce
dbids <dbids@bu.edu>
2019-12-08 17:01:20 -0500

    Cleaned up top a bit

--- top_wack_a_mole.v
+++ top_wack_a_mole.v
@@ -20,18 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 //Partial top module, 5 second countdown not fully implemented
-module top_wack_a_mole(clk, reset, switch_in, digit_select, seven_seg, mux_to_led, level_select);
+module top_wack_a_mole(clk, reset, switch_in, digit_select, seven_seg, led_out, level_select);
     output [6:0] seven_seg;
     output [7:0] digit_select;
-    output [4:0] mux_to_led;
+    output [4:0] led_out;
     input        clk, reset, level_select;
     input  [4:0] switch_in;
+	
+	//Internal wires
     wire check_to_counter, clock_lHz, clock_lkHz, clock_4Hz, clock_choice;
     wire [1:0] game_begin;
     wire [3:0] dc_to_seven;
     wire [31:0] counter_to_mux, mux_to_bcd, bcd_to_dc, count_down, latch_out;
-    wire [4:0] button_in_db, led_out;
-    wire [4:0] led_wait=5'b00000;
+    wire [4:0] button_in_db, led_to_mux, led_wait=5'b00000;
       
     //Establish the clocks based on the clock divider modules
     clock_divider_1Hz cd1(.clock(clk), .reset(reset), .new_clock(clock_lHz));
@@ -42,8 +43,8 @@ module top_wack_a_mole(clk, reset, switch_in, digit_select, seven_seg, mux_to_le
     assign clock_choice = (level_select) ? clock_4Hz : clock_lHz;
     
     //Create the 30 second countdown path
-    topRand tr(.clk(clock_choice), .reset(reset), .displayL(led_out));
-    checkInput ci(.reset(reset), .rand_in(led_out), .switch_in(switch_in), .out(check_to_counter));
+    topRand tr(.clk(clock_choice), .reset(reset), .displayL(led_to_mux));
+    checkInput ci(.reset(reset), .rand_in(led_to_mux), .switch_in(switch_in), .out(check_to_counter));
     counter32 c(.count(counter_to_mux), .reset(reset), .inc(check_to_counter), .clock(clock_choice));
     
     //Create the 5 second countdown path
@@ -55,7 +56,7 @@ module top_wack_a_mole(clk, reset, switch_in, digit_select, seven_seg, mux_to_le
     //Switch between the 5 second countdown and 30 second game and the end game score
     gameBeginControl gb(.reset(reset), .game_begin(game_begin), .clk(clock_lHz));
     assign mux_to_bcd = (game_begin==2'b00) ? count_down : (game_begin==2'b01)? counter_to_mux: latch_out;
-    assign mux_to_led = (game_begin==1'b1)? led_out : led_wait;
+    assign led_out = (game_begin==1'b1)? led_to_mux : led_wait;
     
     //Conver the score to BCD
     binary2BCD bcd(mux_to_bcd, bcd_to_dc);
